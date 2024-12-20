d904b5af8fa2f368e217d5197def646eea31698b
Soham Sarkar <sohamsarkarv1.0@gmail.com>
2024-12-21 05:04:14 +0530

    Whitelisted 5173

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.use((req, res, next) => {
     "https://gdgk-devfest24-scanner.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Origin", "https://devfest.gdgkolkata.in");
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
